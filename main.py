7d7d1ae095830dbfd05593f57d287b261af21dd5
JacksonKehoe <62122050+JacksonKehoe@users.noreply.github.com>
2020-10-03 14:38:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-
+from sklearn.tree import DecisionTreeClassifier 
+from sklearn import metrics
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.tree import DecisionTreeRegressor
