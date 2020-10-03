f3e6d662b17bb16c1bc40f071ed210056a17fa38
JacksonKehoe <62122050+JacksonKehoe@users.noreply.github.com>
2020-10-03 14:42:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -24,3 +24,12 @@ def standardize(data):
     
     return scaler.fit_transform(data)
     
+    ''' need to set X to columns
+    set y to sneaker
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1) #splitting into 70% training 30% test
+    clf = DecisionTreeClassifier()
+    clf = clf.fit(X_train,y_train)
+    y_pred = clf.predict(X_test)
+    print("Accuracy:",metrics.accuracy_score(y_test, y_pred)) #can use to test accuracy
+    '''
+    
