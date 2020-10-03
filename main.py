d66735b40e43c22926ca58e3cfbc3f2e4443f06a
JacksonKehoe <62122050+JacksonKehoe@users.noreply.github.com>
2020-10-03 14:59:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -24,12 +24,40 @@ def standardize(data):
     
     return scaler.fit_transform(data)
     
-    ''' need to set X to columns
-    set y to sneaker
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1) #splitting into 70% training 30% test
-    clf = DecisionTreeClassifier()
-    clf = clf.fit(X_train,y_train)
-    y_pred = clf.predict(X_test)
-    print("Accuracy:",metrics.accuracy_score(y_test, y_pred)) #can use to test accuracy
+# Before we move on to the next step we will seperate the dataset into train, validation, test split
+
+
+# Function from https://datascience.stackexchange.com/questions/15135/train-test-validation-set-splitting-in-sklearn
+def data_split(examples, labels, train_frac, random_state=5):
+    ''' https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
+    param data:       Data to be split
+    param train_frac: Ratio of train set to whole dataset
+
+    Randomly split dataset, based on these ratios:
+        'train': train_frac
+        'valid': (1-train_frac) / 2
+        'test':  (1-train_frac) / 2
+
+    Eg: passing train_frac=0.8 gives a 80% / 10% / 10% split
     '''
+
+    X_train, X_tmp, Y_train, Y_tmp = train_test_split(
+        examples, labels, train_size=train_frac, random_state=random_state)
+
+    X_val, X_test, Y_val, Y_test = train_test_split(X_tmp,
+                                                    Y_tmp,
+                                                    train_size=0.5,
+                                                    random_state=random_state)
+
+    return X_train, X_val, X_test, Y_train, Y_val, Y_test
+
+tree = DecisionTreeRegressor()
+tree.fit(X_train, Y_train)
+tree_predictios = tree.predict(X_val)
+print("Validation")
+scores(Y_val, tree_predictios)
+
+# print("\nTest")
+# tree_predictions_test = tree.predict(X_test)
+# scores(Y_test, tree_predictions_test)
     
